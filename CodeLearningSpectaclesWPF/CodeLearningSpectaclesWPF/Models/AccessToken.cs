6024a5789b9065d51cb0f6ec17455d7050f6af6b
Daniel Pienaar <daniel.pienaar@bbd.co.za>
2024-04-05 09:01:54 +0200

    initial commit

---
+++ CodeLearningSpectaclesWPF/CodeLearningSpectaclesWPF/Models/AccessToken.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeLearningSpectaclesWPF.Models
+{
+  internal class AccessToken
+  {
+    public string access_token {  get; set; } = string.Empty;
+    public string token_type { get; set; } = string.Empty;
+    public string scope { get; set; } = string.Empty;
+  }
+}
---
+++ CodeLearningSpectaclesWPF/CodeLearningSpectaclesWPF/Models/DeviceVerification.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeLearningSpectaclesWPF.Models
+{
+  internal class DeviceVerification
+  {
+    public string device_code { get; set; } = string.Empty;
+    public string user_code { get; set; } = string.Empty;
+    public string verification_uri { get; set; } = string.Empty;
+    public int expires_in { get; set; }
+    public int interval { get; set; }
+  }
+}
