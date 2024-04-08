049f6cf7a6a1b6b00a5e7fbb7bdb9da045e51516
Daniel Pienaar <daniel.pienaar@bbd.co.za>
2024-04-08 16:18:49 +0200

    initial commit

---
+++ CodeLearningSpectaclesWPF/CodeLearningSpectaclesWPF/Models/Profile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeLearningSpectaclesWPF.Models
+{
+  class Profile
+  {
+    public int Profileid { get; set; }
+    public string Name { get; set; }
+    public object[] profilelanguageconstructs { get; set; }
+  }
+
+}
---
+++ CodeLearningSpectaclesWPF/CodeLearningSpectaclesWPF/ViewModels/LoginViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeLearningSpectaclesWPF.ViewModels
+{
+  internal class LoginViewModel : INotifyPropertyChanged
+  {
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private string _urlMessage;
+    public string UrlMessage
+    {
+      get
+      {
+        return _urlMessage;
+      } 
+      
+      set
+      {
+        _urlMessage = value;
+        NotifyPropertyChanged();
+      }
+
+    }
+    private string _messageStart;
+    public string MessageStart
+    { 
+      get
+      {
+        return _messageStart;
+      }
+      set
+      {
+        _messageStart = value;
+        NotifyPropertyChanged();
+      }
+    }
+    private string _messageEnd;
+    public string MessageEnd
+    { 
+      get
+      {
+        return _messageEnd;
+      }
+      set
+      {
+        _messageEnd = value;
+        NotifyPropertyChanged();
+      }
+    }
+
+    public LoginViewModel()
+    {
+      UrlMessage = "";
+      MessageStart = "";
+      MessageEnd = "";
+    }
+
+    private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+  }
+}
