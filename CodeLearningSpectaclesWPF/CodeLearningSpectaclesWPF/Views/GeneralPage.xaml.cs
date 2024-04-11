e7cece1d37dfade18b592632b8998e62c48b9c00
Daniel Pienaar <daniel.pienaar@bbd.co.za>
2024-04-11 14:21:57 +0200

    added search bar

--- CodeLearningSpectaclesWPF/CodeLearningSpectaclesWPF/Views/GeneralPage.xaml.cs
+++ CodeLearningSpectaclesWPF/CodeLearningSpectaclesWPF/Views/GeneralPage.xaml.cs
@@ -29,129 +29,139 @@ namespace CodeLearningSpectaclesWPF.Views
             this.profileLanguageconstructs = profileLanguageconstructs;
 
             InitializeComponent();
-
-            foreach (var dto in languageConstructDTOs)
-            {
-                StackPanel stackPanel = new StackPanel();
-                StackPanel buttonsPanel = new StackPanel();
-
-                Label codeLabel = new Label();
-                codeLabel.Content = dto.Codeconstruct;
-                codeLabel.FontSize = 20;
-                codeLabel.FontWeight = FontWeights.Bold;
-
-                TextBlock constructTextBlock = new TextBlock();
-                constructTextBlock.Text = dto.Construct;
-                constructTextBlock.FontSize = 16;
-                constructTextBlock.Padding = new Thickness(5, 3, 5, 3);
-                constructTextBlock.TextWrapping = TextWrapping.Wrap;
-
-                Border horizontalLine = new Border();
-                horizontalLine.BorderThickness = new Thickness(0, 1, 0, 0);
-                horizontalLine.BorderBrush = Brushes.Black;
-                horizontalLine.Margin = new Thickness(15);
-
-                Button copyButton = new Button();
-                copyButton.Content = "Copy Code";
-                copyButton.Margin = new Thickness(5);
-                copyButton.Background = Brushes.White;
-                copyButton.Click += (sender, e) =>
-                {
-                    string textToCopy = constructTextBlock.Text;
-                    Clipboard.SetText(textToCopy);
-                    MessageBox.Show("Copied to clipboard!", "Copied", MessageBoxButton.OK, MessageBoxImage.Information);
-                };
-
-                Button favouriteButton = new Button();
-                var favButtonId = "favouriteButton" + dto.Languageconstructid.ToString();
-               
-                favouriteButton.Content = !profileLanguageconstructs.Any(plc => plc.Languageconstructid == dto.Languageconstructid) ? "Add to Favourites" : "Saved";
-                favouriteButton.Margin = new Thickness(5);
-                favouriteButton.Padding = new Thickness(3);
-                favouriteButton.IsEnabled = !profileLanguageconstructs.Any(plc => plc.Languageconstructid == dto.Languageconstructid);
-                favouriteButton.Background = Brushes.White;
-                favouriteButton.Click += async (sender, e) =>
-                {
-                    Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Environment.GetEnvironmentVariable("ACCESS_TOKEN"));
-
-                    Profilelanguageconstruct profileLanguageConstruct = new Profilelanguageconstruct
-                    {
-
-                        Languageconstructid = dto.Languageconstructid,
-                        Profileid = Convert.ToInt32(Helpers.ProfileID),
-                    };
-
-                    try
-                    {
-                        var response = await Client.PostAsJsonAsync("Profilelanguageconstructs", profileLanguageConstruct);
-
-                        if (response.IsSuccessStatusCode)
-                        {
-                            MessageBox.Show("Construct successfully saved!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
-                            favouriteButton.Content = "Saved";
-                            favouriteButton.IsEnabled = false;
-
-                            var buttonId = "favouriteNoteButton" + dto.Languageconstructid.ToString();
-                            Button favNoteButton = buttons[buttonId];
-                            favNoteButton.IsEnabled = false;
-                            favNoteButton.Content = "Saved";
-
-                        }
-
-                    }
-                    catch (Exception ex)
-                    {
-                        MessageBox.Show("There was an error saving your selection. Please try again.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                    }
-                   
-                };
-                buttons.Add(favButtonId, favouriteButton);
-
-                Button favouriteNoteButton = new Button();
-                var favNoteButtonId = "favouriteNoteButton" + dto.Languageconstructid.ToString();
-                favouriteNoteButton.Name = favNoteButtonId;
-                favouriteNoteButton.Content = !profileLanguageconstructs.Any(plc => plc.Languageconstructid == dto.Languageconstructid) ? "Save with Note" : "Saved";
-                favouriteNoteButton.Margin = new Thickness(5);
-                favouriteNoteButton.Padding = new Thickness(3);
-                favouriteNoteButton.IsEnabled = !profileLanguageconstructs.Any(plc => plc.Languageconstructid == dto.Languageconstructid);
-                favouriteNoteButton.Background = Brushes.White;
-                favouriteNoteButton.Click += async (sender, e) =>
-                {
-                    inputLabel.Content = dto.Languageconstructid;
-                    popup.IsOpen = true;
-                   
-                };
-                buttons.Add(favNoteButtonId, favouriteNoteButton);
-
-
-                buttonsPanel.Orientation = Orientation.Horizontal;
-                buttonsPanel.Children.Add(favouriteButton);
-                buttonsPanel.Children.Add(favouriteNoteButton);
-                buttonsPanel.Children.Add(copyButton);
-
-                stackPanel.Children.Add(codeLabel);
-                stackPanel.Children.Add(constructTextBlock);
-                stackPanel.Children.Add(horizontalLine);
-                stackPanel.Children.Add(buttonsPanel);
-                stackPanel.Background = Brushes.White;
-
-                Border border = new Border();
-                border.BorderBrush = Brushes.Black;
-                border.Margin = new Thickness(5);
-
-                border.BorderThickness = new Thickness(1);
-                border.Padding = new Thickness(5);
-                border.Child = stackPanel;
-
-                Heading.Content = dto.Codinglanguage == "CSharp" ? "C#" : dto.Codinglanguage;
-                MainWrapPanel.Children.Add(border);
-
-
-            }
-
-
+            LoadData();
         }
 
+        private void LoadData(string search = "")
+        {
+            foreach (var dto in languageConstructDTOs)
+            {
+                if (search.Equals("") || dto.Codeconstruct.Contains(search, StringComparison.OrdinalIgnoreCase))
+                {
+                    StackPanel stackPanel = new StackPanel();
+                    StackPanel buttonsPanel = new StackPanel();
+
+                    Label codeLabel = new Label();
+                    codeLabel.Content = dto.Codeconstruct;
+                    codeLabel.FontSize = 20;
+                    codeLabel.FontWeight = FontWeights.Bold;
+
+                    TextBlock constructTextBlock = new TextBlock();
+                    constructTextBlock.Text = dto.Construct;
+                    constructTextBlock.FontSize = 16;
+                    constructTextBlock.Padding = new Thickness(5, 3, 5, 3);
+                    constructTextBlock.TextWrapping = TextWrapping.Wrap;
+
+                    Border horizontalLine = new Border();
+                    horizontalLine.BorderThickness = new Thickness(0, 1, 0, 0);
+                    horizontalLine.BorderBrush = Brushes.Black;
+                    horizontalLine.Margin = new Thickness(15);
+
+                    Button copyButton = new Button();
+                    copyButton.Content = "Copy Code";
+                    copyButton.Margin = new Thickness(5);
+                    copyButton.Background = Brushes.White;
+                    copyButton.Click += (sender, e) =>
+                    {
+                        string textToCopy = constructTextBlock.Text;
+                        Clipboard.SetText(textToCopy);
+                        MessageBox.Show("Copied to clipboard!", "Copied", MessageBoxButton.OK, MessageBoxImage.Information);
+                    };
+
+                    Button favouriteButton = new Button();
+                    var favButtonId = "favouriteButton" + dto.Languageconstructid.ToString();
+
+                    favouriteButton.Content = !profileLanguageconstructs.Any(plc => plc.Languageconstructid == dto.Languageconstructid) ? "Add to Favourites" : "Saved";
+                    favouriteButton.Margin = new Thickness(5);
+                    favouriteButton.Padding = new Thickness(3);
+                    favouriteButton.IsEnabled = !profileLanguageconstructs.Any(plc => plc.Languageconstructid == dto.Languageconstructid);
+                    favouriteButton.Background = Brushes.White;
+                    favouriteButton.Click += async (sender, e) =>
+                    {
+                        Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Environment.GetEnvironmentVariable("ACCESS_TOKEN"));
+
+                        Profilelanguageconstruct profileLanguageConstruct = new Profilelanguageconstruct
+                        {
+
+                            Languageconstructid = dto.Languageconstructid,
+                            Profileid = Convert.ToInt32(Helpers.ProfileID),
+                        };
+
+                        try
+                        {
+                            var response = await Client.PostAsJsonAsync("Profilelanguageconstructs", profileLanguageConstruct);
+
+                            if (response.IsSuccessStatusCode)
+                            {
+                                MessageBox.Show("Construct successfully saved!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                                favouriteButton.Content = "Saved";
+                                favouriteButton.IsEnabled = false;
+
+                                var buttonId = "favouriteNoteButton" + dto.Languageconstructid.ToString();
+                                Button favNoteButton = buttons[buttonId];
+                                favNoteButton.IsEnabled = false;
+                                favNoteButton.Content = "Saved";
+
+                            }
+
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("There was an error saving your selection. Please try again.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+
+                    };
+                    buttons.Add(favButtonId, favouriteButton);
+
+                    Button favouriteNoteButton = new Button();
+                    var favNoteButtonId = "favouriteNoteButton" + dto.Languageconstructid.ToString();
+                    favouriteNoteButton.Name = favNoteButtonId;
+                    favouriteNoteButton.Content = !profileLanguageconstructs.Any(plc => plc.Languageconstructid == dto.Languageconstructid) ? "Save with Note" : "Saved";
+                    favouriteNoteButton.Margin = new Thickness(5);
+                    favouriteNoteButton.Padding = new Thickness(3);
+                    favouriteNoteButton.IsEnabled = !profileLanguageconstructs.Any(plc => plc.Languageconstructid == dto.Languageconstructid);
+                    favouriteNoteButton.Background = Brushes.White;
+                    favouriteNoteButton.Click += async (sender, e) =>
+                    {
+                        inputLabel.Content = dto.Languageconstructid;
+                        popup.IsOpen = true;
+
+                    };
+                    buttons.Add(favNoteButtonId, favouriteNoteButton);
+
+
+                    buttonsPanel.Orientation = Orientation.Horizontal;
+                    buttonsPanel.Children.Add(favouriteButton);
+                    buttonsPanel.Children.Add(favouriteNoteButton);
+                    buttonsPanel.Children.Add(copyButton);
+
+                    stackPanel.Children.Add(codeLabel);
+                    stackPanel.Children.Add(constructTextBlock);
+                    stackPanel.Children.Add(horizontalLine);
+                    stackPanel.Children.Add(buttonsPanel);
+                    stackPanel.Background = Brushes.White;
+
+                    Border border = new Border();
+                    border.BorderBrush = Brushes.Black;
+                    border.Margin = new Thickness(5);
+
+                    border.BorderThickness = new Thickness(1);
+                    border.Padding = new Thickness(5);
+                    border.Child = stackPanel;
+
+                    Heading.Content = dto.Codinglanguage == "CSharp" ? "C#" : dto.Codinglanguage;
+                    MainWrapPanel.Children.Add(border);
+                }
+            }
+        }
+
+        private void txtSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Heading.Content = "";
+            MainWrapPanel.Children.Clear();
+            buttons.Clear();
+            LoadData(txtSearch.Text);
+        }
 
         private async void SaveButton_Click(object sender, RoutedEventArgs e)
         {
@@ -177,8 +187,8 @@ namespace CodeLearningSpectaclesWPF.Views
                     popup.IsOpen = false;
                     MessageBox.Show("Construct successfully saved!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
                     var favButtonId = "favouriteButton" + inputLabel.Content;
-                    var favNoteButtonId ="favouriteNoteButton" + inputLabel.Content;
-                    
+                    var favNoteButtonId = "favouriteNoteButton" + inputLabel.Content;
+
                     Button favButton = buttons[favButtonId];
                     favButton.IsEnabled = false;
                     favButton.Content = "Saved";
@@ -197,11 +207,6 @@ namespace CodeLearningSpectaclesWPF.Views
                 popup.IsOpen = false;
                 MessageBox.Show("There was an error saving your selection. Please try again.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
-
-
-           
-
-
         }
 
         private void CancelButton_Click(object sender, RoutedEventArgs e)
